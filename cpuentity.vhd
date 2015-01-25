8c4c6dd4b9fe88ff28cab3b252a7f25768f601b6
Jeremie_Astray <5490584810@163.COM>
2015-01-25 10:08:40 +0800

    old

---
+++ cpuentity.vhd
@@ -0,0 +1,344 @@
+--CPUEntity.vhd- CPU模块
+--------------------------------------------------------
+--将取指、译码、执行、访存、写回五个功能模块通过元件例化
+--配置连接起来，构成一个完整的CPU
+--------------------------------------------------------
+--2004-09-04
+library ieee;
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+use work.unitPack.all;
+
+entity CPUEntity is	
+	port(	reset	:in     std_logic;                    --复位
+	        clk		:in 	std_logic;			          --时钟
+			OuterDB	:INOUT	std_logic_vector(15 downto 0);--内存数据线 
+			memAddr	:out	std_logic_vector(15 downto 0);--内存地址线 			
+			wr		:out 	std_logic;                    --内存读写使能
+			flag    :out    std_logic_vector(3 downto 0); --状态位CZVS					
+			RegSel	:in		std_logic_vector(5 downto 0); --寄存器选择					
+			RegOut	:out	std_logic_vector(15 downto 0) --寄存器值输出
+		);
+end CPUEntity;
+
+architecture CPUArch of CPUEntity is   
+	--*** to IF Unit 
+	signal  s_i_PCPlusOffset	: word;
+	signal  s_PCStall 		: std_logic;
+	signal  s_IFFlush 		: std_logic; 
+	signal  s_tempZ,s_tempC	: std_logic;  
+	--*** to ID Unit  	
+	signal  s_PCInc1        : word;
+	signal  s_d_IR			: word;	 	
+	signal  s_w_WBData		: word;
+	signal  s_w_destReg		: std_logic_vector(3 downto 0);
+	signal  s_w_wRegEn		: std_logic;   
+	signal  s_RegSel		: std_logic_vector(3 downto 0);
+	signal  s_RegOut		: word;		
+	--*** to Ex Unit
+	signal  s_e_SA			: std_logic_vector(3 downto 0);
+	signal  s_e_SB			: std_logic_vector(3 downto 0);	
+	signal  s_e_RAOut		: word;
+    signal  s_e_RBOut		: word;
+	signal  s_e_IMM			: word;
+	signal  s_e_ALUSrc      : std_logic_vector(2 downto 0);
+	signal  s_e_ALUOpr      : std_logic_vector(3 downto 0);
+	signal  s_e_SetFlag     : std_logic_vector(2 downto 0);
+    signal	s_e_wrMem		: std_logic_vector(1 downto 0);
+    signal	s_e_wRegEn      : std_logic;
+	signal  s_e_destReg		: std_logic_vector(3 downto 0);
+	signal  s_e_MemToReg    : std_logic;
+	signal  s_forwardA		: std_logic_vector(1 downto 0);
+	signal  s_forwardB		: std_logic_vector(1 downto 0);	   
+	--*** to MemAccess Unit
+	signal  s_PCaddr		: word;	
+	signal  s_m_ALUOut		: word;
+	signal  s_m_RBdata      : word;
+	signal  s_m_wrMem 		: std_logic_vector(1 downto 0);
+	signal  s_m_wRegEn		: std_logic;
+	signal  s_m_memToReg	: std_logic;
+	signal  s_m_destReg		: std_logic_vector(3 downto 0);	
+	--*** to Forwarding Unit  
+	signal 	s_w_ALUOut		 : word; 		
+	signal  s_m_SA			 : std_logic_vector(3 downto 0); 
+	signal  s_w_SA			 : std_logic_vector(3 downto 0);	
+	--*** to WB Unit  
+	signal s_w_memToReg	     : std_logic;  --选择回写的数据源(s_w_ALUOut、OuterDB) 
+	signal s_m_flag          : std_logic_vector(3 downto 0);
+	--*** to WB	  
+	signal s_w_flag          : std_logic_vector(3 downto 0);
+	signal  s_w_MemOut		 : word;
+	signal  s_w_wrMem 		 : std_logic_vector(1 downto 0);
+   
+    ------------------------------------- 
+	component IFEntity is    --取指模块
+	port (	reset		: in  STD_LOGIC;	
+			clk			: in  STD_LOGIC;
+			Z			: in  std_logic; --状态位
+			C           : in  std_logic; 
+			tempZ	    : in  std_logic; --临时状态位,傍路处理	
+			tempC       : in  std_logic; 			
+			e_setFlag	: in  std_logic_vector(2 downto 0);
+			PCPlusOffset: in  word;			--PC + Offset
+			PCStall 	: in  std_logic;	--'1' : PC 保持不变  
+			IFFlush		: in  std_logic;    --'1' : NOP->IR	 
+			OuterDB		: in  word;	
+			PC_addr		: out word; 	    --PC作为内存地址输出,用于下一节拍的取指
+			d_PCInc1 	: out word;			--PC + 1
+			d_IR   		: out word 			--指令寄存器输出          	
+	);
+	end component IFEntity;  
+	
+	component IDEntity is    --译码模块
+	port(	reset	: in std_logic;
+			clk		: in std_logic;	 			
+			d_IR	: in word;	   
+			d_PCInc1 : in word;	
+			--*** for Regs Bank	 
+			w_WBData  : in word;	 			
+			w_destReg : in std_logic_vector(3 downto 0);  -- Destination Reg index
+			w_wRegEn  : in std_logic;  -- '1' enable   			
+			e_SA,e_SB	: out std_logic_vector(3 downto 0);	
+			i_PCPlusOffset : out word;
+			--*** for ALU	 
+			e_RAOut,e_RBOut	: out word;	
+			e_IMM		: out word;	
+			e_ALUSrc	: out std_logic_vector(2 downto 0);
+			e_ALUOpr	: out std_logic_vector(3 downto 0);
+			e_SetFlag	: out std_logic_vector(2 downto 0);	
+			--*** for Memory 
+			e_wrMem	: out std_logic_vector(1 downto 0); --"00" write, "01" read, "1-" do nothing
+			--*** for Regs
+			e_wRegEn  : out  std_logic;		--寄存器写使能 
+			e_destReg : out  std_logic_vector(3 downto 0); 
+			e_MemToReg : out std_logic;	    --内存写入寄存器使能  			
+			--Cpu Interface ---------------------------------
+			RegSel 	: in std_logic_vector(3 downto 0);
+			RegOut	: out word
+			);
+	end component ;	 
+	
+	component ExEntity is   --执行模块
+	port(	reset,clk   : in std_logic;
+			--*** Input for ALU		        
+			e_RAOut,e_RBOut	: in word;	 			
+			e_ALUSrc		: in std_logic_vector(2 downto 0);
+			e_ALUOpr		: in std_logic_vector(3 downto 0);
+			e_SetFlag		: in std_logic_vector(2 downto 0);	
+			e_IMM			: in word;			
+					
+		    forwardA,forwardB  : in  std_logic_vector(1 downto 0);	
+			--*** for Forwarding  
+			e_SA		: in std_logic_vector(3 downto 0);
+			m_SA    	: out std_logic_vector(3 downto 0);		
+			
+			e_ALUOut  	: in word;
+			w_WBdata  	: in word;			
+			m_ALUOut	: out word;
+			m_RBdata  	: out word;	  
+			i_tempZ	    : out std_logic;
+			i_tempC     : out std_logic;
+			--*** Control Signal for Ex_MemReg & Mem_WBReg 
+			m_flag		: out std_logic_vector(3 downto 0);
+			e_wRegEn  	: IN  std_logic; 
+			m_wRegEn 	: out std_logic; 
+			e_memToReg  : in std_logic;
+			m_memToReg  : out std_logic;
+			e_destReg 	: IN  std_logic_vector(3 downto 0); 
+			m_destReg 	: out std_logic_vector(3 downto 0);			
+			e_wrMem		: IN std_logic_vector(1 downto 0); --"00" write, "01" read, "1-" do nothing  			
+			m_wrMem	  	: out std_logic_vector(1 downto 0) --"00" write, "01" read, "1-" do nothing					
+		);
+	end component;  
+		
+	component MemAccessEntity is  --访存模块
+	port(	reset,clk	: in std_logic;
+			m_wrMem		: in std_logic_vector(1 downto 0);
+			w_wrMem     : out std_logic_vector(1 downto 0);
+			m_ALUOut	: in word;    -- Memory addr
+			m_RBdata	: in word;
+			wr			: out std_logic; --内存读写控制 '1':read , '0':write
+			OuterDB		: inout word;
+			w_ALUOut	: out word;	
+			w_MemOut	: out word;
+			-----------------
+			m_flag		: in std_logic_vector(3 downto 0);	   
+			w_flag		: out std_logic_vector(3 downto 0);
+			----------
+			PC          : in  word;
+			addr        : out word;
+			----------
+			--*** for Forwarding 
+			m_SA		: in std_logic_vector(3 downto 0);
+			w_SA		: out std_logic_vector(3 downto 0);	
+			
+			--*** for WB Reg
+			m_wRegEn  	: in std_logic; 
+			m_destReg 	: in std_logic_vector(3 downto 0); 
+			m_memToReg  : in std_logic;
+			
+			--*** for WB Reg
+			w_wRegEn 	: out std_logic; 
+			w_destReg	: out std_logic_vector(3 downto 0); 
+			w_memToReg  : out std_logic
+		);
+	end component;
+	
+	component ForwardingEntity	is     --数据相关检测及傍路处理模块
+	port(	m_wRegEn		: in  std_logic;
+			w_wRegEn		: in  std_logic;
+			m_SA			: in  std_logic_vector(3 downto 0);
+			w_SA			: in  std_logic_vector(3 downto 0);
+			e_SA			: in  std_logic_vector(3 downto 0);
+			e_SB			: in  std_logic_vector(3 downto 0);				
+			forwardA		: out std_logic_vector(1 downto 0);	
+			forwardB		: out std_logic_vector(1 downto 0)
+		);
+	end component ;	
+	
+	component HazardDetectEntity is   --控制相关检测及处理模块 
+	port(	m_wrMem	: in std_logic_vector(1 downto 0);
+			w_wrMem : in std_logic_vector(1 downto 0);
+			d_IR	: in word;
+			IFFlush	: out std_logic;
+			PCStall : out std_logic
+		);
+	end component;	
+		
+begin  
+	--一级流水,取指
+	IFUnit: IFEntity port map (reset=>reset,
+								clk=>clk,								
+								Z=>s_m_flag(2),
+								C=>s_m_flag(3),
+								tempZ=>s_tempZ,
+								tempC=>s_tempC,								
+								e_setFlag=>s_e_SetFlag,
+								PCPlusOffset=>s_i_PCPlusOffset,
+								PCStall=>s_PCStall,
+								IFFlush=>s_IFFlush,	
+								OuterDB=>OuterDB,	
+								PC_addr=>s_PCaddr,
+								d_PCInc1=>s_PCInc1,
+								d_IR=>s_d_IR							
+							);
+	--二级流水,译码			
+	IDUnit: IDEntity port map (	reset=>reset,	
+								clk=>clk,
+                     			d_IR=>s_d_IR,
+								d_PCInc1=>s_PCInc1,	
+								w_WBData=>s_w_WBData,
+								w_destReg=>s_w_destReg,
+								w_wRegEn=>s_w_wRegEn,
+								i_PCPlusOffset=>s_i_PCPlusOffset,
+							    e_SA=>s_e_SA,
+								e_SB=>s_e_SB,
+								e_RAOut=>s_e_RAOut,
+								e_RBOut=>s_e_RBOut,
+								e_IMM =>s_e_IMM,
+								e_ALUSrc=>s_e_ALUSrc,
+								e_ALUOpr=>s_e_ALUOpr,
+								e_SetFlag=>s_e_SetFlag,
+								e_wrMem=>s_e_wrMem,
+								e_wRegEn=>s_e_wRegEn,
+								e_destReg=>s_e_destReg,
+								e_MemToReg=>s_e_MemToReg,  							
+								RegSel=>s_RegSel,
+								RegOut=>s_RegOut
+							);
+	--三级流水,执行/计算有效地址						
+    ExUnit: ExEntity port map(	reset=>reset,
+								clk=>clk,
+	      					   	e_RAOut=>s_e_RAOut,
+								e_RBOut=>s_e_RBOut,
+								e_ALUSrc=>s_e_ALUSrc,
+								e_ALUOpr=>s_e_ALUOpr,
+								e_SetFlag=>s_e_SetFlag,
+								e_IMM =>s_e_IMM, 
+								e_wrMem=>s_e_wrMem,
+								e_wRegEn=>s_e_wRegEn,
+								e_destReg=>s_e_destReg,
+								e_MemToReg=>s_e_MemToReg,
+								forwardA=>s_forwardA,
+								forwardB=>s_forwardB,
+								e_ALUOut=>s_m_ALUOut,
+								e_SA=>s_e_SA,								
+							    m_SA=>s_m_SA,						
+								m_ALUOut=>s_m_ALUOut,
+								w_WBData=>s_w_WBData,
+								m_flag=>s_m_flag,		 
+								i_tempZ=>s_tempZ,
+								i_tempC=>s_tempC,
+								m_RBdata=>s_m_RBdata,
+								m_wrMem =>s_m_wrMem,
+								m_wRegEn=>s_m_wRegEn,
+								m_memToReg=>s_m_memToReg,
+								m_destReg=>s_m_destReg
+							);	 
+	--四级流水,访问内存						
+	MemAccessUnit: MemAccessEntity	port map (reset=>reset,
+											  clk=>clk,
+											  m_wrMem=>s_m_wrMem,
+											  w_wrMem=>s_w_wrMem,
+											  m_ALUOut=>s_m_ALUOut,
+											  m_RBdata=>s_m_RBdata,	
+											  wr=>wr,
+											  OuterDB=>OuterDB,
+											  w_ALUOut=>s_w_ALUOut,
+											  w_MemOut=>s_w_Memout,
+											  m_flag=>s_m_flag,
+											  w_flag=>s_w_flag,
+											  --
+											  PC=>s_PCaddr,
+											  addr=>memAddr,
+											  --
+											  m_SA=>s_m_SA,
+											  w_SA=>s_w_SA,	  					  
+											  m_wRegEn=>s_m_wRegEn,
+											  m_destReg=>s_m_destReg,
+											  m_memToReg=>s_m_memToReg,
+											  w_wRegEn=>s_w_wRegEn,
+											  w_destReg=>s_w_destReg,
+											  w_memToReg=>s_w_memToReg
+											 );	 
+	--***************************************
+	--五级流水,回写寄存器
+    with s_w_memToReg select	--选择回写的数据源
+		s_w_WBData  <= 	s_w_ALUOut  when '1',
+						s_w_MemOut	when others;
+	process(clk)
+	begin
+		if FALLING_EDGE(clk) then
+			flag <= s_w_flag;
+		end if;
+	end process;
+	--***************************************	  
+
+	--傍路处理
+	ForwardUnit: ForwardingEntity port map(	m_wRegEn=>s_m_wRegEn,
+											w_wRegEn=>s_w_wRegEn,
+											m_SA=>s_m_SA,
+											w_SA=>s_w_SA,
+											e_SA=>s_e_SA,
+											e_SB=>s_e_SB,
+											forwardA=>s_forwardA,
+											forwardB=>s_forwardB
+										);
+	--结构、控制相关的处理
+	HDUnit: HazardDetectEntity port map ( m_wrMem=>s_m_wrMem,
+										  w_wrMem=>s_w_wrMem,
+										  d_IR=>s_d_IR, 							    
+		 								  IFFlush=>s_IFFlush,
+										  PCStall=>s_PCStall
+										); 	
+	-- debug	
+	process(RegSel,s_d_IR,s_PCAddr,s_RegOut)
+	begin
+		case RegSel is
+		when "111111" => RegOut <= s_d_IR;
+		when "111110" => RegOut <= s_PCAddr; 
+		when others   => s_RegSel <= RegSel(3 downto 0);  RegOut <= s_RegOut;
+		end case;
+	end process;
+	-------	
+end architecture ;
---
+++ identity.vhd
@@ -0,0 +1,146 @@
+--IDEntity.vhd- 译码模块
+----------------------------------------------------------------------
+--读取寄存器值和指令译码。采取一次译码，逐级传递的方式，译出后几级流水
+--所需的控制信号和数据（如立即数等），在每次时钟上升沿到来时送入下一级
+----------------------------------------------------------------------
+--2004-09-04
+library IEEE;
+use IEEE.std_logic_1164.all; 
+use IEEE.std_logic_unsigned.all;
+use work.unitpack.all;
+
+entity IDEntity is
+	port(	reset			: in std_logic;
+			clk				: in std_logic;	 			
+			d_IR			: in word;	 
+			d_PCInc1 		: in word;	 
+			--*** for Regs Bank	 
+			w_WBData  		: in word;	 			
+			w_destReg 		: in std_logic_vector(3 downto 0);  -- Destination Reg index
+			w_wRegEn  		: in std_logic;  					--寄存器写使能   			
+			e_SA,e_SB		: out std_logic_vector(3 downto 0);	
+			i_PCPlusOffset	: out word;
+			--*** for ALU	 
+			e_RAOut,e_RBOut	: out word;	 						--寄存器A,B输出
+			e_IMM			: out word;	 						--立即数输出
+			e_ALUSrc		: out std_logic_vector(2 downto 0); --运算器选源
+			e_ALUOpr		: out std_logic_vector(3 downto 0); --运算器操作
+			e_SetFlag		: out std_logic_vector(2 downto 0); --状态寄存器写使能
+			--*** for Memory 
+			e_wrMem	: out std_logic_vector(1 downto 0); --"00" write, "01" read, "1-" do nothing
+			--*** for Regs
+			e_wRegEn  : out  std_logic;		--寄存器写使能 
+			e_destReg : out  std_logic_vector(3 downto 0); 
+			e_MemToReg : out std_logic;	    --内存写入寄存器使能 			
+			--Cpu Interface ---------------------------------
+			RegSel 	: in std_logic_vector(3 downto 0);
+			RegOut	: out word
+			);
+end entity ;
+
+architecture IDArch of IDEntity is 
+	SIGNAL RegArray			: REGISTERARRAY; 	-- Regs Array
+    signal wRegIndex,ia,ib	: INDEXTYPE;	
+	--译码生成的控制信号
+	signal SA,SB			: std_logic_vector(3 downto 0);
+	signal wrMem			: std_logic_vector(1 downto 0);
+	signal wRegEn			: std_logic;
+	signal memToReg			: std_logic;   
+	signal offset			: word;
+	signal RA,RB			: word;
+	signal ALUSrc			: std_logic_vector(2 downto 0);
+	signal ALUOpr			: std_logic_vector(3 downto 0);
+	signal SetFlag			: std_logic_vector(2 downto 0);	  
+	signal imm				: word;
+begin 
+	ia <= conv_integer(SA);
+	ib <= conv_integer(SB);
+	RA <= RegArray(ia);     --寄存器A口数据
+	RB <= RegArray(ib);     --寄存器B口数据	
+	--*******************************************
+	--第五级流水,回写寄存器	
+	wRegIndex <= conv_integer(w_destReg);
+	WriteBack:process(reset,clk)   
+	begin	   	
+	    if reset='0' then
+		    RegArray(0) <= x"0000";
+		    RegArray(2) <= x"0000";
+		elsif FALLING_EDGE(clk) and w_wRegEn='1' then
+			RegArray(wRegIndex) <= w_WBData;
+		end if;	 
+	end process; 
+	--*******************************************	
+	--译码模块		
+	Decode_Pro:process(d_IR) 
+	variable op  : std_logic_vector(7 downto 0);
+	variable ctrl:std_logic_vector(21 downto 0);
+	begin 
+		op := d_IR(15 downto 8);  --操作码 
+	    --译码产生的信号:SA,SB,Wrmem,wRegEn,MemToReg,ALUSrc,ALUOpr,SetFlag   
+		case op is
+			when ADD	=> ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"001"&"0000"&"001";
+			when SUBB 	=> ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"001"&"0001"&"001";
+			when DEC    => ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"100"&"0000"&"001";
+			when INC    => ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"010"&"0000"&"001";
+			when ANDins => ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"001"&"0010"&"001";
+			when CMP 	=> ctrl:=d_IR(7 downto 0)&"10"&"0"&"1"&"001"&"0001"&"001";
+			when TEST 	=> ctrl:=d_IR(7 downto 0)&"10"&"0"&"1"&"001"&"0010"&"001";
+			when ORins	=> ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"001"&"0011"&"001";
+			when XORins	=> ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"001"&"0100"&"001";
+			when SHLIns => ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"000"&"0101"&"001";
+			when SHRIns => ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"000"&"0110"&"001";
+			when SAR 	=> ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"000"&"0111"&"001";
+			when MOV 	=> ctrl:=d_IR(7 downto 0)&"10"&"1"&"1"&"011"&"0000"&"000";
+			when LOADH	=> ctrl:="11111111"&"10"&"1"&"1"&"101"&"1000"&"000";	
+						   imm <= "00000000"&d_IR(7 downto 0);	
+			when LOADL	=> ctrl:="11111111"&"10"&"1"&"1"&"101"&"1001"&"000"; 
+						   imm <= "00000000"&d_IR(7 downto 0);
+			when LOAD 	=> ctrl:=d_IR(7 downto 0)&"01"&"1"&"0"&"011"&"0000"&"000";
+			when STORE	=> ctrl:=d_IR(7 downto 0)&"00"&"0"&"1"&"000"&"0000"&"000";			
+			when JR|JRZ|JRNZ|JRC|JRNC =>  ctrl:=DoNothing; --若是JR*指令,计算offset，并向Exe插入Bubble
+					            offset <= d_IR(7)&d_IR(7)&d_IR(7)&d_IR(7)&d_IR(7)&d_IR(7)&d_IR(7)&d_IR(7)&d_IR(7 downto 0);	 		   	
+			when others => ctrl:=DoNothing;
+		 end case;
+		SA 		<= ctrl(21 downto 18);
+		SB 		<= ctrl(17 downto 14);
+		WrMem 	<= ctrl(13 downto 12);
+		wRegEn 	<= ctrl(11);
+		MemToReg<= ctrl(10);
+		ALUSrc 	<= ctrl(9 downto 7);
+		ALUOpr 	<= ctrl(6 downto 3);
+		SetFlag <= ctrl(2 downto 0);
+	end process;
+	--*******************************************
+	--计算跳转时的PC值	  		
+	i_PCPlusOffset <= d_PCInc1 + offset;
+	--*******************************************
+	--流水控制信号传递 ID to Exe
+	process(reset,clk)
+	begin
+		if reset = '0' then     
+			e_wrMem	 <= "10";	-- do nothing		
+			e_wRegEn <= '0'; 			
+		elsif RISING_EDGE(clk) then
+			e_SA <= SA;
+			e_SB <= SB;
+			e_RAOut <= RA;
+			e_RBOut <= RB;
+			e_IMM   <= imm;
+			e_ALUSrc <= ALUSrc;
+			e_ALUOpr <= ALUOpr;
+			e_SetFlag <= SetFlag;
+			e_wrMem  <= wrMem;
+			e_wRegEn <= wRegEn;
+			e_destReg <= SA;
+			e_MemToReg <= memToReg;			
+		end if;				
+	end process;
+	--*******************************************	
+	RegOut <= RegArray(conv_integer(RegSel)); --寄存器输出
+end architecture;
+			
+			
+				
+			
+				
+			
